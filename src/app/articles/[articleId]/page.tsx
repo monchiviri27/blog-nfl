5e52e6f47043ca3979fd83d896a266d9d0b7c2e5
monchiviri27 <monchiviri27@gmail.com>
2025-09-27 19:27:18 +0200

    actualización problemas despliegue en vercel

--- src/app/articles/[articleId]/page.tsx
+++ src/app/articles/[articleId]/page.tsx
@@ -1,7 +1,5 @@
 // src/app/articles/[articleId]/page.tsx - VERSIÓN RESPONSIVA MEJORADA
-//import { getArticleContent, getAllArticleIds, getArticles } from '@/lib/articles';
-import { getArticleData } from "@/lib/articles";
-// Si la función getArticleData es el único import que necesitas de ese archivo.
+import { getArticleContent, getAllArticleIds, getArticles } from '@/lib/articles';
 import { notFound } from 'next/navigation';
 import { MDXRemote } from 'next-mdx-remote/rsc'
 import Image from 'next/image';
